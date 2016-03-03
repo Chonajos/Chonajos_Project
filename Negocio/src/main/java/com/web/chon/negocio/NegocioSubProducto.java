78f2534798d1fcb40152d116d14805350e698e12
shugua <juancruzh91@gmail.com>
2016-03-03 08:29:31 -0600

    se modifico el tipo de id primario

--- Negocio/src/main/java/com/web/chon/negocio/NegocioSubProducto.java
+++ Negocio/src/main/java/com/web/chon/negocio/NegocioSubProducto.java
@@ -12,9 +12,16 @@ import javax.ejb.Remote;
 public interface NegocioSubProducto {
     
     public List<Object[]> getSubProductos();
-    public int deleteSubProducto(int idProducto);
+    public int deleteSubProducto(String idProducto);
     public int insertarSubProducto(Subproducto subProducto);
     public int updateSubProducto(Subproducto subProducto);
+    
+    /**
+     * Devuelve el id del siguiente id a registrar en productos 
+     * @param idCategoria
+     * @return 
+     */
+    public int getLastIdProducto(String idCategoria);
     
 
 }
